72d473c7695442dcd96c7634ce61d4cb075ade54
Faranaz08 <faranazannigeri3142@gmail.com>
2023-10-01 18:59:51 +0530

    third commit

---
+++ Task3 of DOM/dom.js
@@ -0,0 +1,32 @@
+//Examine the document object 
+
+// console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+
+// //document.title =123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent ='Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+//Get element by ID
+//console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title')
+// console.log(headerTitle);
+// headerTitle.textContent='Hello';
+// headerTitle.innerText='goodbye';
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML='<h3>Hello</h3>';
+headerTitle.style.border='2px solid black';
+const addItemButton = document.getElementById("add-item-button");
+addItemButton.style.fontWeight = "bold";
+addItemButton.style.color = "green";
