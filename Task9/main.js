34687346b7b95d86c322470cec13ca12dd1cdac6
Faranaz08 <faranazannigeri3142@gmail.com>
2023-10-02 10:30:01 +0530

    storing multiple users in ls

--- Task9/main.js
+++ Task9/main.js
@@ -24,21 +24,61 @@
 //         emailInput.value = '';
 //     }
 // }
-var li = document.createElement('li');
-li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+// var li = document.createElement('li');
+// li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
 
 // //adding values to local storage
 // localStorage.setItem('name', nameInput.value);
 // localStorage.setItem('email', emailInput.value);
 
 // storing values in the form of object
-var myobj = {
-    name: nameInput.value,
-    email: emailInput.value
-};
-// converting to centarlised
-var myobj_centralised = JSON.stringify(myobj);
-localStorage.setItem("details", myobj_centralised);
-userList.appendChild(li);
-nameInput.value = '';
-emailInput.value = '';
+// var myobj = {
+//     name: nameInput.value,
+//     email: emailInput.value
+// };
+// // converting to centarlised
+// var myobj_centralised = JSON.stringify(myobj);
+// localStorage.setItem("details", myobj_centralised);
+// userList.appendChild(li);
+// nameInput.value = '';
+// emailInput.value = '';
+
+var myForm = document.querySelector('#my-form');
+var nameInput = document.querySelector('#name');
+var emailInput = document.querySelector('#email');
+var msg = document.querySelector('.msg');
+var userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', ONsubmit);
+
+function ONsubmit(e) {
+    e.preventDefault();
+    if (nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all';
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        // //adding values to local storage
+        // localStorage.setItem('name', nameInput.value);
+        // localStorage.setItem('email', emailInput.value);
+
+        // storing values in the form of object
+        var myobj = {
+            name: nameInput.value,
+            email: emailInput.value
+        };
+        // converting to centarlised
+
+        var myobj_centralised = JSON.stringify(myobj);
+        // using email id as key
+        localStorage.setItem(JSON.stringify(emailInput.value), myobj_centralised);
+
+        // appending the info
+        userList.appendChild(li);
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
